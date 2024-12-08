fc997917d2df1d2c48fb5a99a4bee3b9028c3898
Yuri Spizhovyi <spizhov22y@gmail.com>
2024-12-08 12:54:01 -0800

    added docstrings and unittests to mystery_2 and mystery_3

--- 3_documenting_and_testing/exercises/mystery_2.py
+++ 3_documenting_and_testing/exercises/mystery_2.py
@@ -1,5 +1,33 @@
-def mystery_2(a):
+"""
+    Created on 2024-12-08
+    
+    @author: Yurii Spizhovyi
+"""
+from typing import Optional
+
+def mystery_2(a: str) -> Optional[int]:
+    """Returns a length of string
+    
+    Parameters:
+        a string: str, greater than or equal to zero
+    Returns -> int greater or equals zero or None
+    
+    >>> print(mystery_2(""))
+    None
+    
+    >>> mystery_2("a")
+    1
+    
+    >>> mystery_2("ab")
+    2
+    
+    >>> mystery_2(1) # doctest: +IGNORE_EXCEPTION_DETAIL
+    Traceback (most recent call last):
+    TypeError: object of type 'int' has no len()
+    """
     if len(a) == 0:
         return None
 
     return len(a)
+
+mystery_2("")
--- 3_documenting_and_testing/exercises/tests/test_mystery_3.py
+++ 3_documenting_and_testing/exercises/tests/test_mystery_3.py
@@ -3,4 +3,20 @@ import unittest
 from ..mystery_3 import mystery_3
 
 class TestMystery3(unittest.TestCase):
-    """ """
+    """Test the mystery_3 function """
+    
+    def test_1(self):
+        actual = mystery_3(1, 2)
+        expected = 1
+        self.assertEqual(actual, expected)
+
+        
+    def test_2(self):
+        actual = mystery_3(3, 2)
+        expected = 2
+        self.assertEqual(actual, expected)
+        
+    def test_3(self):
+        actual = mystery_3(3, 3)
+        expected = 6
+        self.assertEqual(actual, expected)
