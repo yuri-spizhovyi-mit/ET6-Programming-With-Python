4c83a0ae59bc9cc6ed6e3ef68f4701471f174563
Yuri Spizhovyi <spizhov22y@gmail.com>
2024-12-12 00:56:46 -0800

    changed docstring exclusive_org: deleted duplication

--- 5_tdd_with_llms/exercises/exclusive_or.py
+++ 5_tdd_with_llms/exercises/exclusive_or.py
@@ -6,22 +6,6 @@ Created on 2024-12-12
 @author: Yurii Spizhovyi + Chat GPT
 """
 
-""" Exclusive Or
-
-Write a function that takes in a string and two lists of strings. 
-It will return true if the item is in _only one_ of the lists.
-
-Parameters:
-item: str
-first: list[str]
-second: list[str]
-
-Returns: True if the 'item' is only in list 'first' or is only in list 'second'
-        False if the 'item' in both lists at the same time or does not exists in lists at all
-        
-
-"""
-
 
 def exclusive_or(item, first, second):
     """Exclusive Or
