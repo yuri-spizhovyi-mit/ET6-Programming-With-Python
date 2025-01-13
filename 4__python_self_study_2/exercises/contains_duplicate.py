aca198b6ce5b2d1eadaa5489a44e5c6122819c4a
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-01-12 19:02:18 -0800

    add contains duplicate function

---
+++ 4__python_self_study_2/exercises/contains_duplicate.py
@@ -0,0 +1,9 @@
+from typing import List
+
+class Solution:
+    def containsDuplicate(self, nums: List[int]) -> bool:
+        return len(set(nums)) != len(nums)
+    
+
+result = Solution().containsDuplicate([1,2,3,1])
+print(result) # True
