6e18e4991073bfae41e4e35211fcc0f5970c50b1
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-02-16 09:46:21 -0800

    sandbox

--- mit_6001x/unit_2/sandbox.py
+++ mit_6001x/unit_2/sandbox.py
@@ -1,16 +1,24 @@
-# Paste your code into this box 
-#s = 'azcbobobegghakl'
-s = 'sadfanabgsyoiaiwon'
-sub = []
-longest_sub = []
-for char in s:
-    if len(sub) == 0:
-        sub.append(char)
-    elif char > sub[-1]:
-        sub.append(char)
+# Paste your code into this box
+# s = 'azcbobobegghakl'
+
+s = 'bobegghakl'
+s = 'zyx'
+s = 'cqnqiehzynwwre'
+
+sub = ''
+longest_sub = ''
+
+if len(s) == 1:
+    print("Longest substring in alphabetical order is: " + str(longest_sub))
+sub = s[0]
+longest_sub = s[0]
+for i in range(len(s) - 1):
+    if s[i + 1] >= s[i]:
+        sub = sub + s[i + 1]
         if len(longest_sub) < len(sub):
-            longest_sub = sub
+            longest_sub = sub[:]
     else:
-        sub = []
-sub_sting = ''.join(longest_sub)
-print('Longest substring in alphabetical order is: ' + sub_sting)
+        sub = s[i+1]
+        if len(longest_sub) < len(sub):
+            longest_sub = sub[:]
+print("Longest substring in alphabetical order is: ", longest_sub)
