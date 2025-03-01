1133a7ed83c28c5ad1fb277a243f30d78fd47589
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-02-28 23:50:39 -0800

    add exceptions lesson

---
+++ 2__python_self_study_1/mit/exceptions.py
@@ -0,0 +1,10 @@
+try:
+  a = int(input("A: "))
+  b = int(input("B: "))
+  print(a/b)
+  print("Okay")
+except ValueError:
+  print("Could not convert to a number")
+except ZeroDivisionError:
+  print("Bug in user input")
+print("Outside")
