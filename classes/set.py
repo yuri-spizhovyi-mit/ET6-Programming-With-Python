a05c9b6844e2f081e940b07d9fdef826841d16ae
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-09 20:12:44 -0700

    add coordinate

--- classes/set.py
+++ classes/set.py
@@ -1,10 +1,14 @@
 class IntSet(object):
   def __init__(self):
+    """Create an empty set of integers"""
     self.vals = []
   def insert(self, e):
+    """Assumes e is an integer and inserts e into self""" 
     #if e not in self.vals:
     self.vals.append(e)
   def member(self, e):
+    """Assumes e is an integer
+      Returns True if e is in self, and False otherwise"""    
     return e in self.vals
   def remove(self, e):
     try:
@@ -17,13 +21,24 @@ class IntSet(object):
       for e in self.vals:
         result = result + str(e) + ',' 
       return '{' + result[:-1] + '}'
+  
+  def intersect(self, other):
+      common_elements = IntSet()
+      for e in self.vals:
+        if e in other.vals:
+          common_elements.insert(e) 
+        return common_elements
 
-s = IntSet()
+  def __len__(self):
+        return len(self.vals)
 
-s.insert(3)
-s.insert(4)
-s.insert(3)
-s.remove(3)
-s.remove(6)
-print(s)
-print(s.member(3))
+
+s1 = IntSet()
+s2 = IntSet()
+s1.insert(3)
+s1.insert(4)
+s2.insert(3)
+s2.insert(5)
+
+
+print(s1.intersect(s2))
