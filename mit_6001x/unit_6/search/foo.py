518aec714e3d7dcbbaae55704d988038cef6d659
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-15 16:53:51 -0700

    add foo

---
+++ mit_6001x/unit_6/search/foo.py
@@ -0,0 +1,11 @@
+e = 5
+L = [1, 2, 3, 4, 5]
+def search(L, e):
+    for i in range(len(L)):
+        if L[i] == e:
+            return True
+        elif L[i] > e:
+            return False
+    return False
+
+search(L, e)
