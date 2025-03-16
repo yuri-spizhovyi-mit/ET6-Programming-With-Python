f0c4c61f037a49897d4c376d79a3c16e060fd387
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-16 11:18:16 -0700

    add merging sort

---
+++ mit_6001x/unit_6/search/merging_sort.py
@@ -0,0 +1,17 @@
+def merge(left, right):
+  result = []
+  i, j = 0, 0
+  while i < len(left) and j < len(right):
+    if left[i] < right[i]:
+      result.append(left[i])
+      i += 1
+    else:
+      result.append(right[i])
+      j += 1
+  while (i < len(left)):
+    result.append(left[i])
+    i += 1
+  while (j < len(right)):
+    result.append(right[j])
+    j += 1
+  return result
