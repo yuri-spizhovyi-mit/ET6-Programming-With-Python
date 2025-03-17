3edf8743adba5397f842cf38bcf1c6877ab28057
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-16 19:49:35 -0700

    add set_6

--- mit_6001x/unit_6/search/merge_recursive.py
+++ mit_6001x/unit_6/search/merge_recursive.py
@@ -2,11 +2,11 @@ def merge(left, right):
     result = []
     i, j = 0, 0
     while i < len(left) and j < len(right):
-        if left[i] < right[i]:
+        if left[i] < right[j]:
             result.append(left[i])
             i += 1
         else:
-            result.append(right[i])
+            result.append(right[j])
             j += 1
     while i < len(left):
         result.append(left[i])
@@ -15,3 +15,17 @@ def merge(left, right):
         result.append(right[j])
         j += 1
     return result
+
+
+def merge_sort(L):
+    if len(L) < 2:
+        return L[:]
+    else:
+        middle = len(L) // 2
+        left = merge_sort(L[:middle])
+        right = merge_sort(L[middle:])
+        return merge(left, right)
+
+
+L = [3, 2, 1, 1, 2, 3]
+print(merge_sort(L))
--- mit_6001x/unit_6/search/selection_sort_new.py
+++ mit_6001x/unit_6/search/selection_sort_new.py
@@ -1,6 +1,6 @@
 def newSort(L):
     for i in range(len(L) - 1):
-        j=i+1
+        j = i + 1
         while j < len(L):
             if L[i] > L[j]:
                 temp = L[i]
