4423d2d707fc35a5678da7e711cd76d24f68c6e6
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-17 20:29:35 -0700

    add plot example

---
+++ mit_6001x/unit_6/visualizing/example_1.py
@@ -0,0 +1,25 @@
+import pylab as plt
+
+my_samples = []
+my_linear = []
+my_quadratic = []
+my_cubic = []
+my_exponential = []
+
+for i in range(0, 30):
+    my_samples.append(i)
+    my_linear.append(i)
+    my_quadratic.append(i**2)
+    my_cubic.append(i**3)
+    my_exponential.append(1.5**i)
+
+plt.plot(my_samples, my_linear, label='Linear')
+plt.plot(my_samples, my_quadratic, label='Quadratic')
+plt.plot(my_samples, my_cubic, label='Cubic')
+plt.plot(my_samples, my_exponential, label='Exponential')
+
+plt.legend()       
+plt.xlabel('Samples')
+plt.ylabel('Values')
+plt.title('Different Growth Rates')
+plt.show()         
