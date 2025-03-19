ac8c2d496a169d40df3f78f7bf806b8e0e387bbb
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-18 21:22:15 -0700

    add title and labels for plot example

--- mit_6001x/unit_6/visualizing/example_1.py
+++ mit_6001x/unit_6/visualizing/example_1.py
@@ -13,9 +13,19 @@ for i in range(0, 30):
     my_cubic.append(i**3)
     my_exponential.append(1.5**i)
 
+plt.figure("lin")
+plt.xlabel("Sample points")
+plt.ylabel("linear function")
+plt.title("Different Growth Rates")
 plt.plot(my_samples, my_linear, label="Linear")
+plt.figure("quad")
+plt.xlabel("Sample points")
+plt.ylabel("quad function")
+plt.title("Different Growth Rates")
 plt.plot(my_samples, my_quadratic, label="Quadratic")
+plt.figure("cube")
 plt.plot(my_samples, my_cubic, label="Cubic")
+plt.figure("expo")
 plt.plot(my_samples, my_exponential, label="Exponential")
 
 plt.legend()  # Optional, to see labels
