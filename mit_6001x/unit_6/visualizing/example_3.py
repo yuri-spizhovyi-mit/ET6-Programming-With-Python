f7ddd49f97fdbddaaa5719363588944b03119b6a
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-03-19 19:06:01 -0700

    add example 3

--- mit_6001x/unit_6/visualizing/example_3.py
+++ mit_6001x/unit_6/visualizing/example_3.py
@@ -13,26 +13,20 @@ for i in range(0, 30):
     my_cubic.append(i**3)
     my_exponential.append(1.5**i)
 
-plt.figure("lin quad")
+plt.figure("cube exp log")
 plt.clf()
-plt.subplot(211)
-plt.ylim(0, 900)
-plt.plot(my_samples, my_linear, "b-", label="Linear", linewidth=2.0)
-plt.subplot(212)
-plt.ylim(0, 900)
-plt.plot(my_samples, my_quadratic, "ro", label="Quadratic", linewidth=3.0)
-plt.legend(loc="upper left")
-plt.title("Linears vs. Quadratic")
-
-plt.figure("cube exp")
-plt.clf()
-plt.subplot(121)
-plt.ylim(0, 140000)
 plt.plot(my_samples, my_cubic, "g^", label="Cubic", linewidth=4.0)
-plt.subplot(122)
-plt.ylim(0, 140000)
 plt.plot(my_samples, my_exponential, "r--", label="Exponential", linewidth=5.0)
+plt.yscale("log")
 plt.legend()
 plt.title("Cubic vs. Exponential")
 
-plt.show()  # 🔥 This line is what shows the plot window
+
+plt.figure("cube exp linear")
+plt.clf()
+plt.plot(my_samples, my_cubic, "g--", label="cubic", linewidth=2.0)
+plt.plot(my_samples, my_exponential, "r", label="exponential", linewidth=4.0)
+plt.legend()
+plt.title("Cubic vs. Exponential")
+
+plt.show()
