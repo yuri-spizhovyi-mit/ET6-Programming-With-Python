c9bced6a7f770983ae1cee7ea0faa44506a6b1bc
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-05 14:26:56 -0700

    add histogram

---
+++ mit_6002x/unit_2/histogram.py
@@ -0,0 +1,13 @@
+import pylab
+import random
+
+vals = []
+for i in range(1000):
+  num1 = random.choice(range(0, 101))
+  num2 = random.choice(range(0, 101))
+  vals.append(num1 + num2)
+pylab.hist(vals, bins=10)
+pylab.ylabel("Number of Occurences")
+pylab.xlabel("Sum of Two Random Integers (0–100)")
+pylab.title("Histogram of Sums from Two Random Integers")
+pylab.show()
