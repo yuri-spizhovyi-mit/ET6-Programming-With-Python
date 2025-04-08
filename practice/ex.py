ee5273aa0a77f1d10f2d73f95358921767851629
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-08 01:49:48 -0700

    add graphs practice

--- practice/ex.py
+++ practice/ex.py
@@ -1,3 +1,6 @@
+from black import get_sources
+
+
 class Node:
     """Class Node"""
 
@@ -24,7 +27,7 @@ class Edge:
         self.dest = dest
 
     def get_source(self):
-        return self.scr
+        return self.src
 
     def get_destination(self):
         return self.dest
@@ -55,3 +58,63 @@ class WeightedEdge(Edge):
 
 e1 = WeightedEdge(a, b, 5)
 print(e1)
+
+
+class Digraph:
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+
+    def add_node(self, node):
+        if node in self.nodes:
+            raise ValueError("Duplicate node")
+        self.nodes.append(node)
+        self.edges[node] = []
+
+    def add_edge(self, edge):
+        src = edge.get_source()
+        dest = edge.get_destination()
+        if not (src in self.nodes and dest in self.nodes):
+            raise ValueError("Node not in graph")
+        self.edges[src].append(dest)
+
+    def children_of(self, node):
+        return self.edges[node]
+
+    def has_node(self, node):
+        return node in self.nodes
+
+    def __str__(self):
+        result = ""
+        for src in self.nodes:
+            for dest in self.edges[src]:
+                result += src.get_name() + "->" + dest.get_name() + "\n"
+        return result[:-1]
+
+
+dg = Digraph()
+a = Node("A")
+b = Node("B")
+dg.add_node(a)
+dg.add_node(b)
+e = Edge(a, b)
+dg.add_edge(e)
+print(dg)  # Output: A->B
+
+
+class Graph(Digraph):
+    def add_edge(self, edge):
+        Digraph.add_edge(self, edge)
+        rev = Edge(edge.get_destination(), edge.get_source())
+        Digraph.add_edge(self, rev)
+
+
+g = Graph()
+a = Node("A")
+b = Node("B")
+g.add_node(a)
+g.add_node(b)
+ge = Edge(a, b)
+g.add_edge(ge)
+print("Undirected Graph")
+print(g)
