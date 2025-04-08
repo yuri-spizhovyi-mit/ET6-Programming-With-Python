36c20d7def6275135f420c83a467179473d0a2c6
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-07 23:57:09 -0700

    add weighted graph

--- practice/graph.py
+++ practice/graph.py
@@ -33,3 +33,20 @@ a = Node("A")
 b = Node("B")
 e = Edge(a, b)
 print(e)
+
+
+class WeightedEdge(Edge):
+    def __init__(self, src, dest, weight=1.0):
+        self.src = src
+        self.dest = dest
+        self.weight = weight
+
+    def getWeight(self):
+        return self.weight
+
+    def __str__(self):
+        return self.src.getName() + "->(" + str(self.weight) + ")" + self.dest.getName()
+
+
+e = WeightedEdge(a, b, 5)
+print(e)  # Output: A->(5)B
