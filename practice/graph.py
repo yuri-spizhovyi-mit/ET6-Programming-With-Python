9f9f50860ac8adbd24de3e6f4e66e3cbfbcc6d1a
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-07 23:57:32 -0700

    add digraph

--- practice/graph.py
+++ practice/graph.py
@@ -50,3 +50,45 @@ class WeightedEdge(Edge):
 
 e = WeightedEdge(a, b, 5)
 print(e)  # Output: A->(5)B
+
+
+class Digraph(object):
+    def __init__(self):
+        self.nodes = []
+        self.edges = {}
+
+    def addNode(self, node):
+        if node in self.nodes:
+            raise ValueError("Duplicate node")
+        self.nodes.append(node)
+        self.edges[node] = []
+
+    def addEdge(self, edge):
+        src = edge.getSource()
+        dest = edge.getDestination()
+        if not (src in self.nodes and dest in self.nodes):
+            raise ValueError("Node not in graph")
+        self.edges[src].append(dest)
+
+    def childrenOf(self, node):
+        return self.edges[node]
+
+    def hasNode(self, node):
+        return node in self.nodes
+
+    def __str__(self):
+        result = ""
+        for src in self.nodes:
+            for dest in self.edges[src]:
+                result += src.getName() + "->" + dest.getName() + "\n"
+        return result[:-1]  # Remove the last newline
+
+
+g = Digraph()
+a = Node("A")
+b = Node("B")
+g.addNode(a)
+g.addNode(b)
+e = Edge(a, b)
+g.addEdge(e)
+print(g)  # Output: A->B
