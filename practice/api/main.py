117e80a0e5916e391bf6ef980e12c168c11076d3
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-10 00:01:44 -0700

    add main fix

--- practice/api/main.py
+++ practice/api/main.py
@@ -1,5 +1,11 @@
-from api.github import GitHubClient
-from api.weather import WeatherClient
+from github import GitHubClient
+from dotenv import load_dotenv
+import os
+from weather import WeatherClient
+
+load_dotenv()
+
+API_KEY = os.getenv("WEATHER_API_KEY")
 
 # GitHub API call
 github = GitHubClient("https://api.github.com")
@@ -7,7 +13,9 @@ repos = github.get_user_repos("octocat")
 for repo in repos[:3]:
     print("🔹", repo["name"])
 
-# Weather API call (you need a real API key)
-weather = WeatherClient("https://api.openweathermap.org", "YOUR_API_KEY")
+weather = WeatherClient("https://api.openweathermap.org", API_KEY)
 forecast = weather.get_weather("London")
-print("🌤️ Temp:", forecast["main"]["temp"], "°C")
+
+print("🌤️ Weather in London:")
+print("Temperature:", forecast["main"]["temp"], "°C")
+print("Condition:", forecast["weather"][0]["description"])
