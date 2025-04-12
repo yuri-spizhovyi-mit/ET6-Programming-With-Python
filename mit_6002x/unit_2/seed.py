f47941990e347a9d40a81ace386f1157a21ebf65
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-11 23:20:25 -0700

    add ps2 problem

---
+++ mit_6002x/unit_2/seed.py
@@ -0,0 +1,12 @@
+import random
+
+random.seed(9001)
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
+print(random.randint(1, 10))
