ecdbe56815cfa809da290b04f255ea0e97db4640
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-17 23:58:14 -0700

    add dfs training

--- practice/dfs_train.py
+++ practice/dfs_train.py
@@ -1,8 +1,19 @@
-def dfs(digraph, start, end, path, shortest):
+from graph import Digraph
+
+
+def dfs(Digraph, start, end, path, shortest):
     path = path + [start]
     if start == end:
         return path
-    for node in graph.childrenOf(start):
+    for node in Digraph.childrenOf(start):
         if node not in path:
             if shortest == None of len(path) < len(shortest):
-                pass
+                new_path = dfs(Digraph, node, end, path, shortest)
+                if new_path != None:
+                    shortest = new_path
+
+    return shortest
+
+
+def shortest(digraph, start, end):
+    return dfs(Digraph, start, end, [], None)
