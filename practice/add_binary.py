d1c5484d109e68021e161ee94dba961b0ba065ad
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-22 23:38:01 -0700

    add binary

---
+++ practice/add_binary.py
@@ -0,0 +1,9 @@
+class Solution:
+    def addBinary(self, a: str, b: str) -> str:
+        a_int = int(a, 2)
+        b_int = int(b, 2)
+        total = a_int + b_int
+        return bin(total)[2:]
+
+s = Solution()
+print(s.addBinary("11", "10"))
