dfdd1ef6a37109f8920efbe7eebf552fd0064947
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-23 23:41:35 -0700

    add Efficient Algorithm (Kadane’s Algorithm)

--- mit_6002x/max_contig_sum.py
+++ mit_6002x/max_contig_sum.py
@@ -1,3 +1,6 @@
+"""Efficient Algorithm (Kadane’s Algorithm)"""
+
+
 def max_contig_sum(L):
     """L, a list of integers, at least one positive
     Returns the maximum sum of a contiguous subsequence in L"""
