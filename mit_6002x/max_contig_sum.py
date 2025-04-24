cde15c92c74ca5938a79bcf51e351601b1f937da
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-23 23:42:28 -0700

    add Efficient Algorithm (Kadane’s Algorithm)

--- mit_6002x/max_contig_sum.py
+++ mit_6002x/max_contig_sum.py
@@ -9,3 +9,7 @@ def max_contig_sum(L):
         current_sum = max(num, current_sum + num)
         max_sum = max(max_sum, current_sum)
     return max_sum
+
+
+L = [3, 4, -1, 5, -4]
+print(max_contig_sum(L))
