e41fefe8eab2f8d7f535db61417c51c0d797444d
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-24 23:20:31 -0700

    add fibonacci iterative

---
+++ practice/fib_iter.py
@@ -0,0 +1,14 @@
+def fib_iter(n):
+    if n == 0:
+        return 0
+    if n == 1:
+        return 1
+
+    prev, cur = 0, 1
+    for _ in range(2, n + 1):
+        prev, cur = cur, prev + cur
+
+    return cur
+
+
+print(fib_iter(7))
