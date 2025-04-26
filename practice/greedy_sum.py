674ffa1d316caabf831e9fd1c8ea8e7fb68cdf8d
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-25 23:48:28 -0700

    add greedy sum

---
+++ practice/greedy_sum.py
@@ -0,0 +1,10 @@
+def greedySum(L, s):
+    total_mult = 0
+    remain = s
+
+    for num in L:
+        mult = remain // num
+        total_mult += mult
+        remain -= num * mult
+
+    return total_mult if remain == 0 else 'no solution'
