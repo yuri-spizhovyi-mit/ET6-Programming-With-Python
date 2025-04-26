7e42b548ea5e3ce3786844ad827e638e147cb73d
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-25 23:40:59 -0700

    add fibonacci dynamic

---
+++ practice/fib_dynamic.py
@@ -0,0 +1,14 @@
+def fib(n):
+    if n <= 1:
+        return n
+
+    dp = [0] * (n + 1)
+    dp[1] = 1
+
+    for i in range(2, n + 1):
+        dp[i] = dp[i - 1] + dp[i - 2]
+
+    return dp[n]
+
+
+print(fib(7))  # Output: 13
