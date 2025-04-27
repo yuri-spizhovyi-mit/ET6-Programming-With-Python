687897f375e8c2d9e1c01a203e376e564463e88f
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-27 00:18:30 -0700

    add dfs

---
+++ mit_6002x/midterm/dfs_recursive.py
@@ -0,0 +1,14 @@
+graph = {"A": ["B", "C"], "B": ["A", "D"], "C": ["A"], "D": ["B"]}
+
+
+def dfs(graph, node, visited=None):
+    if visited is None:
+        visited = set()
+    if node not in visited:
+        print(node)  # Visit the node
+        visited.add(node)
+        for neighbor in graph[node]:
+            dfs(graph, neighbor, visited)
+
+
+dfs(graph, "A")
