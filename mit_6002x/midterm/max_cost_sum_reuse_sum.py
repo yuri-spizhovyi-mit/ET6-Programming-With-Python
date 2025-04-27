d8e6fa17121159c625613d252eadeff41e88c252
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-26 22:51:33 -0700

    add max cost

--- mit_6002x/midterm/max_cost_sum_reuse_sum.py
+++ mit_6002x/midterm/max_cost_sum_reuse_sum.py
@@ -1,13 +1,15 @@
-def max_contig_sum_brute_force(L):
-    max_sum = L[0]  # start with the first element
+def max_contig_sum_reuse_sum(L):
+    max_sum = L[0]
     n = len(L)
     for i in range(n):
+        current_sum = 0
         for j in range(i, n):
-            current_sum = sum(L[i : j + 1])
+            current_sum += L[j]
             if current_sum > max_sum:
                 max_sum = current_sum
+
     return max_sum
 
 
 L = [3, 4, -1, 5, -4]
-print(max_contig_sum_brute_force(L))
+print(max_contig_sum_reuse_sum(L))
