242fdd623ec44ca58a20d3fdfff173f2bf68c9cb
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-04-30 23:33:50 -0700

    add binary search

---
+++ practice/search/binary.py
@@ -0,0 +1,15 @@
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    return -1
+
+
+# Example
+print(binary_search([1, 3, 5, 7, 9], 5))  # Output: 2
