edbf6fee30f258887b5cdf89bc0c28ef7402fbdc
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-05-23 23:31:29 -0700

    add r_1_6 recursive

--- practice/functions/r_1_6.py
+++ practice/functions/r_1_6.py
@@ -4,5 +4,14 @@ def sum_squared_positive_odd(n):
     return sum([x**2 for x in range(1, n) if x % 2 == 1])
 
 
+def sum_squared_positive_odd_recursive(n):
+    if n <= 0:
+        raise ValueError("n must be a positive integer")
+    if n == 1:
+        return 0
+    elif (n - 1) % 2 == 1:
+        return (n - 1) ** 2 + sum_squared_positive_odd_recursive(n - 1)
+
+
 if __name__ == "__main__":
     print(sum_squared_positive_odd(6))
