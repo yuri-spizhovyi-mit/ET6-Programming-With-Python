9bc7d4878b9dda556d0b686a56379df78e2d0b21
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-05-27 22:46:49 -0700

    add read and reverse

---
+++ practice/functions/c_1_22.py
@@ -0,0 +1,15 @@
+def dot_product(l1: list, l2: list):
+    l3 = []
+    for i in range(len(l1)):
+        l3.append(l1[i] * l2[i])
+    return l3
+
+
+print(dot_product([1, 3, 3], [3, 2, 1]))
+
+
+def dot_product_map(l1: list, l2: list):
+    return list(map(lambda a, b: a * b, l1, l2))
+
+
+print(dot_product_map([4, 5, 6], [6, 5, 4]))
