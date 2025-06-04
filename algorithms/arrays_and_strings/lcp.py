be951842531b6f4ec632748940d59f83ba70cd9c
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-06-03 23:23:17 -0700

    add the longest common prefix

---
+++ algorithms/arrays_and_strings/lcp.py
@@ -0,0 +1,13 @@
+def longest_common_prefix(strs: list[str]) -> str:
+    if not strs:
+        return ""
+
+    prefix = strs[0]
+
+    for word in strs[1:]:
+        while not word.startswith(prefix):
+            prefix = prefix[:-1]
+            if not prefix:
+                return ""
+
+    return prefix
