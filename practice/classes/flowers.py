11b41c846902f2bec62b65f28cda3e6beb8064e8
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-06-07 00:15:59 -0700

    add class flowers

--- practice/classes/flowers.py
+++ practice/classes/flowers.py
@@ -1,31 +1,32 @@
 class Flower:
     """Class of flowers"""
+
     def __init__(self, name: str, num_petals: int, price: float):
-        self.name = name
-        self.num_petals = num_petals
-        self.price = price
+        self._name = name
+        self._num_petals = num_petals
+        self._price = price
 
-    def set_name(self, name):
-        self.name = name
+    def set_name(self, name: str) -> None:
+        self._name = name
 
-    def get_name(self):
-        return self.name
+    def get_name(self) -> str:
+        return self._name
 
-    def set_num_petals(self, num_petals):
-        self.num_petals = num_petals
+    def set_num_petals(self, num_petals: int) -> None:
+        self._num_petals = num_petals
 
-    def get_num_petals(self):
-        return self.num_petals
+    def get_num_petals(self) -> int:
+        return self._num_petals
 
-    def set_price(self, price):
-        self.price = price
+    def set_price(self, price: int) -> None:
+        self._price = price
 
-    def get_price(self):
-        return self.price
+    def get_price(self) -> float:
+        return self._price
 
     def __str__(self):
-        return f"The name of flower is {self.name}. It has {self.num_petals} petals. Price is {self.price}"
+        return f"The name of flower is {self._name}. It has {self._num_petals} petals. Price is {self._price}"
 
 
-flower_1 = Flower('rose', 10, 20)
+flower_1 = Flower("rose", 10, 20)
 print(flower_1)
