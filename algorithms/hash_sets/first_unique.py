9fa23116de52cdff0a90c6688d05b57a7a9b8e0a
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-06-07 23:51:17 -0700

    add first unique

---
+++ algorithms/hash_sets/first_unique.py
@@ -0,0 +1,17 @@
+def first_unique(s: str):
+    # First pass: count each character
+    char_count = {}
+    for char in s:
+        char_count[char] = char_count.get(char, 0) + 1
+
+        # Second pass: find the first character with cout 1
+
+    for char in s:
+        if char_count[char] == 1:
+            return char
+    return "_"
+
+
+print(first_unique("leetcode"))  # ➞ "l"
+print(first_unique("loveleetcode"))  # ➞ "v"
+print(first_unique("aabbcc"))  # ➞ "_"
