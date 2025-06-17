638cfee36488df24f0406382774f7298bb65bf50
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-06-16 22:42:00 -0700

    add selection sort desc

---
+++ algorithms/sorting/selection_sort_desc.py
@@ -0,0 +1,14 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n):
+        max_ind = i  # assume first element is minimum
+        for j in range(i + 1, n):
+            if arr[j] > arr[max_ind]:
+                max_ind = j  # update min index if found smaller
+        arr[i], arr[max_ind] = arr[max_ind], arr[i]  # swap
+
+    return arr
+
+
+# Example
+print(selection_sort([29, 10, 14, 37, 13]))  # Output: [10, 13, 14, 29, 37]
