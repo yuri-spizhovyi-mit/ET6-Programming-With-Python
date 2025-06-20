730f2cfd6771ff00c8bc7ac360540ed84dff9997
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-06-19 23:17:19 -0700

    add unique frequency

---
+++ algorithms/sorting/unique_sort_by_freq.py
@@ -0,0 +1,11 @@
+from collections import Counter
+
+def sort_unique_by_frequency(nums):
+    freq = Counter(nums)
+    # Use set(nums) or freq.keys() to get unique elements
+    unique = list(freq.keys())
+    return sorted(unique, key=lambda x: (-freq[x], x))
+
+nums = [4, 5, 6, 5, 4, 3]
+print(sort_unique_by_frequency(nums))
+# ➜ [4, 5, 3, 6]
