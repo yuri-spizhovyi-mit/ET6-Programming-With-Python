5666704ab550c3c8ba4a754c58c9d6fe98a53db9
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-06-23 22:08:55 -0700

    add linear sum

---
+++ algorithms/recursion/linear.py
@@ -0,0 +1,9 @@
+def linear_sum(S, n):
+    """Return the sum of the first n numbers of sequence S"""
+    if n == 0:
+        return 0
+    else:
+        return linear_sum(S, n - 1) + S[n - 1]
+
+S = [1, 2, 3, 4, 5]
+print(linear_sum(S, 3))
