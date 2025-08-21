97286e1d92623fade01bb099cd62a6b495c11037
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-08-20 23:42:22 -0700

    add file system node disk size calculation

--- algorithms/trees/computing_disk_space.py
+++ algorithms/trees/computing_disk_space.py
@@ -8,3 +8,14 @@ class FileSystemNode:
     def add_child(self, child):
         if not self.is_file:
             self.children.append(child)
+
+
+def compute_disk_space(node):
+    """Return total disk space used by the subtree rooted at node."""
+    if node.is_file:
+        return node.size
+
+    total = 0
+    for child in node.children:
+        total += compute_disk_space(child)
+    return total
