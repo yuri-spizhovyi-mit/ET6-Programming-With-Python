b874ce463206f6b6cf807b2ae1ad0ee8fc6abae6
Yuri Spizhovyi <spizhov22y@gmail.com>
2025-08-20 23:45:40 -0700

    add file system node disk size calculation examples

--- algorithms/trees/computing_disk_space.py
+++ algorithms/trees/computing_disk_space.py
@@ -1,9 +1,9 @@
 class FileSystemNode:
     def __init__(self, name, size=0, is_file=False):
         self.name = name
-        self.size = size        # file size if is_file=True
+        self.size = size  # file size if is_file=True
         self.is_file = is_file
-        self.children = []      # only used if it's a folder
+        self.children = []  # only used if it's a folder
 
     def add_child(self, child):
         if not self.is_file:
@@ -19,3 +19,27 @@ def compute_disk_space(node):
     for child in node.children:
         total += compute_disk_space(child)
     return total
+
+
+# Build example tree
+root = FileSystemNode("root")  # folder
+docs = FileSystemNode("Documents")
+pics = FileSystemNode("Pictures")
+
+file1 = FileSystemNode("resume.pdf", size=120, is_file=True)
+file2 = FileSystemNode("book.pdf", size=300, is_file=True)
+file3 = FileSystemNode("photo1.jpg", size=500, is_file=True)
+file4 = FileSystemNode("photo2.jpg", size=700, is_file=True)
+
+# Build hierarchy
+root.add_child(docs)
+root.add_child(pics)
+
+docs.add_child(file1)
+docs.add_child(file2)
+
+pics.add_child(file3)
+pics.add_child(file4)
+
+# Compute total disk usage
+print("Total disk space used:", compute_disk_space(root), "KB")
